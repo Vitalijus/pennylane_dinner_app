c4ffe9ad2579ba52c80444aa06e0d5b1f8209531
Vitalijus Desukas <vitalij.desuk@gmail.com>
2025-05-11 00:10:16 +0300

    prepare to deploy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,34 +2,11 @@ class PagesController < ApplicationController
   require "./app/services/elasticsearch/ingridients_query.rb"
 
   def home
-    # binding.pry
-    # if params[:ingredients].present?
       elasticsearch = Elasticsearch::IngridientsQuery.new(params[:ingredients])
       elasticsearch.response
+
       @total_recipes = elasticsearch.result[:total]
-
-      # binding.pry
-      @recipes = Kaminari.paginate_array(elasticsearch.result[:recipes]).page(params[:page]).per(3)
-      # binding.pry
-
-    # else
-    #   format.html { render :home, status: :unprocessable_entity }
-    # end
-    # else
-    #   redirect_back fallback_location: root_url
-    # end
-
-    # elasticsearch.errors if elasticsearch.errors.present?
-
-    # respond_to do |format|
-    #   if elasticsearch.errors.present?
-    #     format.html { redirect_to root_url, notice: "Vehicle was successfully created." }
-    #     # format.json { render :show, status: :created, location: @vehicle }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @vehicle.errors, status: :unprocessable_entity }
-    #   end
-    # end
+      @recipes = Kaminari.paginate_array(elasticsearch.result[:recipes]).page(params[:page]).per(5)
   end
 
   private

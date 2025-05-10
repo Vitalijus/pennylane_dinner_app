27d0cb7f447a84dec0c9257ef4c0f6a72b11c677
Vitalijus Desukas <vitalij.desuk@gmail.com>
2025-05-10 22:42:31 +0300

    add html and css styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,31 @@ class PagesController < ApplicationController
   require "./app/services/elasticsearch/ingridients_query.rb"
 
   def home
+    # binding.pry
+    # if params[:ingredients].present?
       elasticsearch = Elasticsearch::IngridientsQuery.new(params[:ingredients])
-      @recipes = elasticsearch.response
+      elasticsearch.response
+      @recipes = elasticsearch.result
+      # binding.pry
 
+    # else
+    #   format.html { render :home, status: :unprocessable_entity }
+    # end
+    # else
+    #   redirect_back fallback_location: root_url
+    # end
+
+    # elasticsearch.errors if elasticsearch.errors.present?
+
+    # respond_to do |format|
+    #   if elasticsearch.errors.present?
+    #     format.html { redirect_to root_url, notice: "Vehicle was successfully created." }
+    #     # format.json { render :show, status: :created, location: @vehicle }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @vehicle.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   private

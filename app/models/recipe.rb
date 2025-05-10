8057b8ddfc6eede08f26ea32fbef2f1821f71517
Vitalijus Desukas <vitalij.desuk@gmail.com>
2025-05-10 17:46:49 +0300

    make a remark about setting ELASTICSEARCH_URL as BONSAI_URL in Heroku

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,8 @@ class Recipe < ApplicationRecord
 
   # Elasticsearch
   # Heroku config is set to: ELASTICSEARCH_URL=https://paas:*************b31617@oin-us-east-1.searchly.com
+  # Set ELASTICSEARCH_URL same as BONSAI_URL, when using Bonsai
+  # using following command: heroku config:set ELASTICSEARCH_URL=`heroku config:get BONSAI_URL`
   # Local is set to: ELASTICSEARCH_URL=http://localhost:9200
   # Recipe.__elasticsearch__.create_index! - to create a new index.
   # Recipe.import - to import data.
